07b61499c83d98c39025acafcb36d88bb5293868
zhayujie <yjzha1996@163.com>
2023-03-04 18:10:46 +0800

    fix: use default max_token

--- model/chatgpt/chatgpt_model.py
+++ model/chatgpt/chatgpt_model.py
@@ -76,7 +76,7 @@ class ChatGPTModel(Model):
                 model="text-davinci-003",  # 对话模型的名称
                 prompt=new_query,
                 temperature=0.9,  # 值在[0,1]之间，越大表示回复越具有不确定性
-                max_tokens=4096,  # 回复最大的字符数
+                #max_tokens=4096,  # 回复最大的字符数
                 top_p=1,
                 frequency_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
                 presence_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
