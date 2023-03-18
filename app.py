1c43111b1b1e3b8d56b9a3f167da7b360011058f
RA <pio71ctt@gmail.com>
2023-03-18 22:48:06 +0800

    多通道

--- app.py
+++ app.py
@@ -3,6 +3,13 @@
 import config
 from channel import channel_factory
 from common import log
+from multiprocessing import Process
+
+def startProcess(channel_type):
+        # create channel
+        channel = channel_factory.create_channel(channel_type)
+        # startup channel
+        channel.startup()
 
 if __name__ == '__main__':
     try:
@@ -12,13 +19,10 @@ if __name__ == '__main__':
         model_type = config.conf().get("model").get("type")
         channel_type = config.conf().get("channel").get("type")
 
-        log.info("[INIT] Start up: {} on {}", model_type, channel_type)
-
-        # create channel
-        channel = channel_factory.create_channel(channel_type)
-
-        # startup channel
-        channel.startup()
+        for type in channel_type:
+            log.info("[INIT] Start up: {} on {}", model_type, type)
+            p = Process(target=startProcess, args=(type))
+            p.start()
     except Exception as e:
         log.error("App startup failed!")
         log.exception(e)
