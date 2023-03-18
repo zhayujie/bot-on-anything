0fc54184234fd2ce5a1900a59aa84550acb4d0f5
RA <pio71ctt@gmail.com>
2023-03-19 00:45:46 +0800

    将终端通道放到主进程启动

--- app.py
+++ app.py
@@ -24,13 +24,21 @@ if __name__ == '__main__':
         model_type = config.conf().get("model").get("type")
         channel_type = config.conf().get("channel").get("type")
 
-        # 使用进程池启动子进程
+        # 使用主进程启动终端通道
+        if "terminal" in channel_type:
+            index=channel_type.index("terminal")
+            terminal = channel_type.pop(index)
+        else:
+            terminal = None
+        # 使用进程池启动其他通道子进程
         pool = Pool(len(channel_type))
-
         for type in channel_type:
             log.info("[INIT] Start up: {} on {}", model_type, type)
             pool.apply_async(wrapper, args=[type])
 
+        if terminal:
+            channel = channel_factory.create_channel(terminal)
+            channel.startup()
         # 等待池中所有进程执行完毕
         pool.close()
         pool.join()
