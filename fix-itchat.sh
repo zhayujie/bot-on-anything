ec08165e16f08114e9b2a5a38a6aae341f894d79
牟勇 <my24251325@gmail.com>
2023-03-19 11:48:02 +0800

    fix itchat bug

--- fix-itchat.sh
+++ fix-itchat.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+# @see https://stackoverflow.com/questions/30003570/how-to-use-gnu-sed-on-mac-os-10-10-brew-install-default-names-no-longer-su
+# @see https://www.cnblogs.com/fnlingnzb-learner/p/10657285.html
 cmd=sed
 if [ "$(uname)" == "Darwin" ];then
   brew install gnu-sed
